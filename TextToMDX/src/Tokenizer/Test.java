368dfea10099ea7a3369e99670f85dc8e9275c6b
Xavier Passos Rodrigues <xavier@MacBook-Pro-de-Xavier.local>
2017-05-18 17:23:41 +0200

    alterações no parser

--- TextToMDX/src/Tokenizer/Test.java
+++ TextToMDX/src/Tokenizer/Test.java
@@ -15,18 +15,30 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int i=0;
        String example ="Cubo Adventure Works.\n"+
                "Eixo 0, Medida x, Dimensao y atributo z.\n"+
                "Eixo 1, Medida a, Dimensao y atributo z.\n"+
                "Onde dimensao x atributo y menor 0.";
        
-       Tokenizer tokenizer = new Tokenizer(example);
+       String[] string= example.split("\n");
+       System.out.println(string[0].trim());
+       for(String s: string){
+           System.out.println(i+" -> " +s);
+           i++;
+       
+       
+       }
+       
+       /**Tokenizer tokenizer = new Tokenizer(example);
        
        while(tokenizer.hasNextToken()){
            System.out.println(tokenizer.nextToken().getToken() + tokenizer.nextToken().getType().toString());
        
-       }
+       }*/
        
     }
+    
+   
     
 }
